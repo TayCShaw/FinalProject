ef405cbf4d066aea90583daf622b94b9d16a5f7a
TShaw <47929530+TayCShaw@users.noreply.github.com>
2019-04-27 10:40:55 -0400

    Updated to final

--- FinalProject/Signup.aspx.cs
+++ FinalProject/Signup.aspx.cs
@@ -29,51 +29,57 @@ namespace FinalProject
             SqlCommand count = new SqlCommand("Select max(userID) from Users", connection);
             SqlDataReader reader;
 
-            if (txtPassword.Text.Equals(txtConfirmPassword.Text))
+            if (txtUsername.Text.Length == 0 || txtPassword.Text.Length == 0 || txtConfirmPassword.Text.Length == 0)
             {
-                int newUserID = 0;
-                lblPasswordError.Text = "SAME PASS";
-                try
-                {
-                    connection.Open();
-                    newUserID = (Int32)count.ExecuteScalar() + 1;
-
-                    string passhash = Security.Sha256(txtPassword.Text);
-                    insert.Parameters.AddWithValue("@userid", newUserID);
-                    insert.Parameters.AddWithValue("@username", txtUsername.Text);
-                    insert.Parameters.AddWithValue("@password", passhash);
-                    insert.Parameters.AddWithValue("@time", DateTime.Now);
-
-                    reader = insert.ExecuteReader();
-                    Session["Username"] = txtUsername.Text;
-                    Session["UserID"] = newUserID;
-                    Response.Redirect("Home.aspx");
-                }
-                catch (Exception er)
-                {
-                    lblErrorMessages.Text = er.ToString();
-                }
-                finally
-                {
-                    connection.Close();
-                }
+                // Username, password, or confirm password fields are empty
+                lblErrorMessages.Text = "All fields must be filled.";
             }
             else
             {
-                lblPasswordError.Text = "Passwords do not match!";
-            }
+                if (txtPassword.Text.Equals(txtConfirmPassword.Text))
+                {
+                    int newUserID = 0;
+                    lblPasswordError.Text = "SAME PASS";
+                    try
+                    {
+                        connection.Open();
+                        newUserID = (Int32)count.ExecuteScalar() + 1;
 
-            
+                        string passhash = Security.Sha256(txtPassword.Text);
+                        insert.Parameters.AddWithValue("@userid", newUserID);
+                        insert.Parameters.AddWithValue("@username", txtUsername.Text);
+                        insert.Parameters.AddWithValue("@password", passhash);
+                        insert.Parameters.AddWithValue("@time", DateTime.Now);
+
+                        reader = insert.ExecuteReader();
+                        Session["Username"] = txtUsername.Text;
+                        Session["UserID"] = newUserID;
+                        Response.Redirect("Home.aspx");
+                    }
+                    catch (Exception er)
+                    {
+                        lblErrorMessages.Text = er.ToString();
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+                else
+                {
+                    lblPasswordError.Text = "Passwords do not match!";
+                }
+            }
         }
 
-        
+
         protected void txtUsername_TextChanged(object sender, EventArgs e)
         {
             lblErrorMessages.Text = "";
 
             SqlConnection connection = new SqlConnection(connectionString);
             SqlCommand cmd = new SqlCommand("Select userName from Users where userName = @username", connection);
-            cmd.Parameters.AddWithValue("@username",txtUsername.Text);
+            cmd.Parameters.AddWithValue("@username", txtUsername.Text);
             SqlDataReader reader;
             try
             {
@@ -90,7 +96,7 @@ namespace FinalProject
                 lblUsernameError.Text = "Username available!";
 
             }
-            catch(Exception er)
+            catch (Exception er)
             {
                 lblErrorMessages.Text = er.ToString();
             }
